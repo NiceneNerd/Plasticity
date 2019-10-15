cd63c63346f56742a48a850317dd1a01c4791474
Caleb Smith <calebdixonsmith@gmail.com>
2019-10-14 19:11:36 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('docs/README.md', 'r') as readme:
 
 setup(
     name='plasticity-ai',
-    version='0.1.4',
+    version='0.1.5',
     author='NiceneNerd',
     author_email='macadamiadaze@gmail.com',
     description='An AI program editor for The Legend of Zelda: Breath of the Wild',
